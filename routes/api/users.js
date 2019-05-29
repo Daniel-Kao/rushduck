7ed298583d045416910d24f13bc25a02fb41db25
Daniel-Kao <396007476@qq.com>
2019-05-29 19:09:12 +0800

    2nd

--- routes/api/users.js
+++ routes/api/users.js
@@ -25,7 +25,10 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { name, topup, meal } = req.body;
+    let { name, topup, meal } = req.body;
+
+    topup = topup ? topup : 0;
+    meal = meal ? meal : 0;
 
     const newRecord = { topup, meal };
 
@@ -38,7 +41,7 @@ router.post(
         user = new User({ name });
         user.records.unshift(newRecord);
       }
-      console.log(topup);
+      user.balance = user.balance + Number(topup) - Number(meal);
       await user.save();
       res.json(user);
     } catch (err) {
@@ -61,5 +64,39 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+// @route   GET api/users
+// @desc    delete one user
+// @access  private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    await User.findOneAndDelete({ name: req.body.name });
+
+    res.json({ msg: 'User deleted' })
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Error Error')
+  }
+})
+
+// @route   GET api/users/me
+// @desc    get user recent expenses 
+// @access  private
+
+router.get('/me', auth, async (req, res) => {
+  try {
+    const user = await User.findOne({ name: req.body.name }).sort({ _id: 1 }).limit(10)
+
+    if (!user) {
+      return res.status(400).json({ msg: '该用户不存在' });
+    }
+    res.json(user)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server error')
+  }
+})
+
 
 module.exports = router;

2d41722b2cca220086ad09d00e09916d8a2b7d05
kaojixiang <396007476@qq.com>
2019-08-25 07:30:18 +0800

    haha

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className='navbar bg-dark'>
+    <nav className="navbar bg-dark">
       <h1 style={{ marginBottom: 0 }}>
-        <Link to='/'>
-        </Link>
+        <Link to="/"></Link>
       </h1>
       <ul>
         <li>
-          <Link to='/login'>登陆</Link>
+          <Link to="/login">登 陆</Link>
         </li>
         <li>
-          <Link to='!#'>注销</Link>
+          <Link to="!#">注销</Link>
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
